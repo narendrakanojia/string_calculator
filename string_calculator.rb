d9e8416d7540fbfa0f69c9eb82aacc0b713fd6a5
narendrakanojia <narendra.kanojia@sodelsolutions.com>
2025-07-19 23:20:46 +0530

    syntax error fixes.

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,19 +3,14 @@ class StringCalculator
 		return 0 if numbers.empty?
 		if numbers.start_with?("//")
 			delimiter, numbers = numbers.split("\n", 2)
-			seprator = delimiter[2..]
+			seprator = delimiter[2..-1]
 			number_array = numbers.split(seprator)
-			number_array.sum(&:to_i)
+
 		else
 			number_array = numbers.split(/,|\n/)
 			return number_array[0].to_i if number_array.count == 1
-			number_array.sum(&:to_i)
+			
 		end
+		number_array.sum(&:to_i)
 	end
 end
-
-
-puts StringCalculator.add("")
-puts StringCalculator.add("1")
-puts StringCalculator.add("1,5")
-puts StringCalculator.add("1\n2,3")
