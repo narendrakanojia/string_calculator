19efa1254329a0b535507c4365778cd25c260544
narendrakanojia <narendra.kanojia@sodelsolutions.com>
2025-07-19 19:52:45 +0530

    added changes for examples input

---
+++ string_calculator.rb
@@ -0,0 +1,14 @@
+class StringCalculator
+	def self.add(numbers)
+		return 0 if numbers.empty?
+		number_array = numbers.split(",")
+		return number_array[0].to_i if number_array.count == 1
+
+		number_array.sum(&:to_i)
+	end
+end
+
+
+puts StringCalculator.add("")
+puts StringCalculator.add("1")
+puts StringCalculator.add("1,5")
