813f318b67af690fc415f1bdcb6e96680748873e
narendrakanojia <narendra.kanojia@sodelsolutions.com>
2025-07-19 23:17:47 +0530

    added delimiter changes

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,10 +1,16 @@
 class StringCalculator
 	def self.add(numbers)
 		return 0 if numbers.empty?
-		number_array = numbers.split(/,|\n/)
-		return number_array[0].to_i if number_array.count == 1
-
-		number_array.sum(&:to_i)
+		if numbers.start_with?("//")
+			delimiter, numbers = numbers.split("\n", 2)
+			seprator = delimiter[2..]
+			number_array = numbers.split(seprator)
+			number_array.sum(&:to_i)
+		else
+			number_array = numbers.split(/,|\n/)
+			return number_array[0].to_i if number_array.count == 1
+			number_array.sum(&:to_i)
+		end
 	end
 end
 
